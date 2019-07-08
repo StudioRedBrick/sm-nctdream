65b191312dc3aa6d5a3b3f2a2839b2eaa01a8b02
subin <sbyoon92@gmail.com>
2019-07-08 14:47:24 +0900

    subin commit

---
+++ js/main_m.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    test();
+    centerEle();
+});
+
+function test(){
+    $("#test").wScratchPad({
+        bg: './img/aladdin.jpg',
+        fg: '#aeaeae',
+        size: 10,
+        realtime: true
+    })
+}
+
+
+function centerEle(){
+    var win = $(window).height()/2;
+    var val = $(".content .inner").height()/2;
+    var fin = win-val;
+    
+    $(".content .inner").css({"padding-top":fin});
+    console.log('fin='+fin);
+}
