8a313b572904f2e42235b93f5372bbdd73cd9ea1
subin <sbyoon92@gmail.com>
2019-07-17 17:14:29 +0900

    j

--- js/main.js
+++ js/main.js
@@ -11,11 +11,11 @@ $(document).ready(function(){
 });
 
 function getWidth(){
+    
+     var mobileKeyWords;
     $(window).resize(function(){
-            
+            mobileKeyWords = new Array('iPhone','SAMSUNG','BlackBerry','Android','Windows CE','Windows CE;','LG','MOT','SonyEricsson','Symbian','Opera Mobi','Opera Mini','IEmobile');
             if ($(window).width() < 1336){
-                
-                var mobileKeyWords = new Array('iPhone','SAMSUNG','BlackBerry','Android','Windows CE','Windows CE;','LG','MOT','SonyEricsson','Symbian','Opera Mobi','Opera Mini','IEmobile');
                 for(var word in mobileKeyWords){
                     if(navigator.userAgent.match(mobileKeyWords[word])!=null){
                         return;
@@ -34,7 +34,7 @@ function getWidth(){
         //console.log('window'+$(window).width());
     
         if ($(window).width() < 1336) {       // if width is less than 600px
-           var mobileKeyWords = new Array('iPhone','SAMSUNG','BlackBerry','Android','Windows CE','Windows CE;','LG','MOT','SonyEricsson','Symbian','Opera Mobi','Opera Mini','IEmobile');
+           mobileKeyWords = new Array('iPhone','SAMSUNG','BlackBerry','Android','Windows CE','Windows CE;','LG','MOT','SonyEricsson','Symbian','Opera Mobi','Opera Mini','IEmobile');
             for(var word in mobileKeyWords){
                 if(navigator.userAgent.match(mobileKeyWords[word])!=null){
                     return;
