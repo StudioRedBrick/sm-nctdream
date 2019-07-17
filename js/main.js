581d7aec128e901d94772a7d622a0e4c9ad91f79
subin <sbyoon92@gmail.com>
2019-07-17 16:17:47 +0900

    sdklj

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(window).load(function(){
+    getWidth();
+});
+
+function getWidth(){
+    $(window).resize(function(){
+            
+            if ($(window).width() < 415) {
+                
+                var mobileKeyWords = new Array('iPhone','SAMSUNG','BlackBerry','Android','Windows CE','Windows CE;','LG','MOT','SonyEricsson','Symbian','Opera Mobi','Opera Mini','IEmobile');
+                for(var word in mobileKeyWords){
+                    if(navigator.userAgent.match(mobileKeyWords[word])==null){
+                        location.href="index.html";
+                        break;
+                    }
+                }
+            }
+            else {                           
+               location.href="index.html";
+            }
+        });
+
+        //console.log('window'+$(window).width());
+    
+        if ($(window).width() < 415) {       // if width is less than 600px
+           var mobileKeyWords = new Array('iPhone','SAMSUNG','BlackBerry','Android','Windows CE','Windows CE;','LG','MOT','SonyEricsson','Symbian','Opera Mobi','Opera Mini','IEmobile');
+            for(var word in mobileKeyWords){
+                if(navigator.userAgent.match(mobileKeyWords[word])==null){
+                    location.href="index.html";
+                    break;
+                }
+            } 
+        }
+        else {          
+           location.href="index.html"; 
+        } 
+}
+
+
+
+
+
+
+
+
+
+
--- js/main_17.js
+++ js/main_17.js
@@ -6,6 +6,7 @@ $(window).load(function(){
     
     scratchMe();
     refreshPage();
+    
 
 });
 
