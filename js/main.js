e5922ef0f7aeae34b5939683b39e586dd19ab51a
subin <sbyoon92@gmail.com>
2019-07-17 17:09:23 +0900

    j

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 function getWidth(){
     $(window).resize(function(){
             
-            if ($(window).width() < 1024){
+            if ($(window).width() < 1336){
                 
                 var mobileKeyWords = new Array('iPhone','SAMSUNG','BlackBerry','Android','Windows CE','Windows CE;','LG','MOT','SonyEricsson','Symbian','Opera Mobi','Opera Mini','IEmobile');
                 for(var word in mobileKeyWords){
@@ -26,14 +26,14 @@ function getWidth(){
                 }
             }
         
-        if($(window).width() > 1024){                           
+        if($(window).width() > 1336){                           
                location.href="index.html";
             }
         });
 
         //console.log('window'+$(window).width());
     
-        if ($(window).width() < 1024) {       // if width is less than 600px
+        if ($(window).width() < 1336) {       // if width is less than 600px
            var mobileKeyWords = new Array('iPhone','SAMSUNG','BlackBerry','Android','Windows CE','Windows CE;','LG','MOT','SonyEricsson','Symbian','Opera Mobi','Opera Mini','IEmobile');
             for(var word in mobileKeyWords){
                 if(navigator.userAgent.match(mobileKeyWords[word])!=null){
@@ -44,7 +44,7 @@ function getWidth(){
                 }
             } 
         }
-         if($(window).width() > 1024){                           
+         if($(window).width() > 1336){                           
                location.href="index.html";
             }
 }
